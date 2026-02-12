21d391b4762824e8c09a46fa8853220a643437c8
Bearstorm <bearstormchannel@gmail.com>
2026-02-12 22:16:08 +0100

    Update run.sh

--- xmrig/rootfs/run.sh
+++ xmrig/rootfs/run.sh
@@ -1,4 +1,3 @@
-
 #!/bin/sh
 set -eu
 
@@ -50,6 +49,7 @@ else
 fi
 
 # --- MSR SELF-TEST (container) ---
+# If rdmsr is available, verify we can read 0x1a4 from inside the container.
 if command -v rdmsr >/dev/null 2>&1; then
   echo "[xmrig-addon] MSR self-test: rdmsr 0x1a4"
   if rdmsr 0x1a4 >/dev/null 2>&1; then
@@ -84,5 +84,5 @@ exec /usr/bin/xmrig \
   $TLS_ARGS \
   $THREAD_ARGS \
   $PRIO_ARGS \
-  --no-msr \
   --randomx-mode=fast
+
