cb9b6a6ddccb17e37b73743d6b0e2ecb1ecbefdd
Bearstorm <bearstormchannel@gmail.com>
2026-02-12 17:46:40 +0100

    Update run.sh

--- xmrig/run.sh
+++ xmrig/run.sh
@@ -3,9 +3,11 @@
 CONFIG_PATH=/data/options.json
 
 POOL=$(grep -o '"pool": "[^"]*' $CONFIG_PATH | cut -d'"' -f4)
+PORT=$(grep -o '"port": "[^"]*' $CONFIG_PATH | cut -d'"' -f4)
 USER=$(grep -o '"user": "[^"]*' $CONFIG_PATH | cut -d'"' -f4)
 PASS=$(grep -o '"pass": "[^"]*' $CONFIG_PATH | cut -d'"' -f4)
 
-echo "Starting XMRig on pool: $POOL"
+echo "Starting XMRig on: ${POOL}:${PORT} with TLS enabled"
 
-exec /usr/bin/xmrig --url "$POOL" --user "$USER" --pass "$PASS"
+# Vynútené TLS (SSL) spojenie, ktoré port 443 vyžaduje
+exec /usr/bin/xmrig --url "${POOL}:${PORT}" --user "$USER" --pass "$PASS" --tls
